0d355d94365c8f0ddc71e3a8f033975895ab8929
julialange@gmx.net <>
2018-04-01 18:09:12 +0200

    initiale version

---
+++ media/Scripting/ServerSideScripting/PHP/shop/debug.inc.php
@@ -0,0 +1,11 @@
+<?php
+class DebugConfiguration
+{
+	// Central switch to turn debugging on/off.
+	//
+	// Configures e.g. the PHP debug settings and whether to use
+	// the live PayPal URLs or the Sandbox PayPal URLs.
+
+	public static $debug = false;
+}
+?>
