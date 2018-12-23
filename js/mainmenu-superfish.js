2e8722e206ee29e40a4ba669bb85b1fecdd9402a
nleanba <noam@helou.ch>
2018-12-23 01:16:35 +0100

    updated navbar to be mobile friendly

---
+++ js/mainmenu-superfish.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function () {
+  $("ul.mainmenu").superfish({
+    delay: 400,
+    animation: {
+      show: "show"
+    },
+    dropShadows: false,
+    disableHI: false
+  })
+});*/
